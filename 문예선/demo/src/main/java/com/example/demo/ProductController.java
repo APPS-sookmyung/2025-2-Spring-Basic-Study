a00bf08b9981e088662303fd39674f907b772e4a
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-10-29 23:46:37 +0900

    Feat: 스프링 베이직 4주차 과제 제출

--- 문예선/demo/src/main/java/com/example/demo/ProductController.java
+++ 문예선/demo/src/main/java/com/example/demo/ProductController.java
@@ -1,10 +1,20 @@
 package com.example.demo;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@Controller
+@ResponseBody
 public class ProductController {
     // 상품  조회 및 상품 등록
     ProductController() {
         System.out.println("진짜 될까?");
+    }
+    @RequestMapping(value="", method = RequestMethod.GET)
+    public String getProduct(){
+        return "문예선의 자바 교재";
     }
 }
